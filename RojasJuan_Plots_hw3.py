075dc6e01d22a214f65fd0038455864102731901
juandrojas <juandrojas33@gmail.com>
2018-07-20 20:50:18 -0500

    se agregan subplots ODE

--- RojasJuan_Plots_hw3.py
+++ RojasJuan_Plots_hw3.py
@@ -10,16 +10,32 @@ fig = plt.figure()
 ax = fig.gca(projection='3d')
 
 # Prepare arrays x, y, z
+t = data_ODE[:,0]
 x = data_ODE[:,1]
 y = data_ODE[:,2]
 z = data_ODE[:,3]
 
-fig.suptitle('Trayectoria de la partícula')
+fig.suptitle('Trayectoria de la partícula', fontsize=18)
 ax.plot(x, y, z)
 plt.savefig('ODE.pdf')
 plt.clf()
 plt.close()
 
+#Gráficas Adicionales
+plt.figure(figsize=(8, 12))
+plt.subplot(311)
+plt.plot(y, x)
+plt.title('Gráfica de X vs Y', fontsize=18)
+plt.subplot(312)
+plt.plot(z, x)
+plt.title('Gráfica de X vs Z', fontsize=18)
+plt.subplot(313)
+plt.title('Gráfica de Y vs T', fontsize=18)
+plt.plot(y, t)
+plt.subplots_adjust(hspace=0.5)
+plt.savefig('ODE_additional.pdf')
+plt.clf()
+plt.close()
 #PDE's
 
 #condición inicial
@@ -31,7 +47,7 @@ y = np.arange(0.0, 1.0, 0.01)
 x, y = np.meshgrid(x, y)
 z = data_PDE_init
 ax.plot_surface(x, y, z)
-fig.suptitle('Condición inicial')
+fig.suptitle('Condición inicial', fontsize=18)
 fig.savefig('PDE_initial.pdf')
 plt.clf()
 plt.close()
@@ -45,7 +61,7 @@ y = np.arange(0.0, 1.0, 0.01)
 x, y = np.meshgrid(x, y)
 z = data_PDE_fixed
 ax.plot_surface(x, y, z)
-fig.suptitle('Condición inicial')
+fig.suptitle('Estado en t=60ms', fontsize=18)
 fig.savefig('PDE_fixed.pdf')
 plt.clf()
 plt.close()
