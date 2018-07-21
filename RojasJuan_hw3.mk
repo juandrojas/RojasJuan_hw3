f7b90b56b727845078bab260e2a849686a66b205
juandrojas <juandrojas33@gmail.com>
2018-07-20 23:05:44 -0500

    se completa el .tex hasta los avances actuales, se dita el .mk para agregar el .tex, falta copmpletar .mk con wildcards.

--- RojasJuan_hw3.mk
+++ RojasJuan_hw3.mk
@@ -1,5 +1,7 @@
-all : PDE_initial.pdf PDE_fixed.pdf ODE.pdf ODE_additional.pdf
+RojasJuan_Resultados_hw3.pdf : PDE_initial.pdf PDE_fixed.pdf ODE.pdf ODE_additional.pdf RojasJuan_Resultados_hw3.tex
+	pdflatex $@
 
+#crear archivos .dat si no existen antes de compilar el script .py
 RojasJuan_Plots_hw3.py : data_ODE.dat data_PDE_fixed.dat
 
 ODE.pdf : data_ODE.dat RojasJuan_Plots_hw3.py
