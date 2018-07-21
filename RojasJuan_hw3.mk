77036cd928c9e7d313c38ec32be879744ea86e13
juandrojas <juandrojas33@gmail.com>
2018-07-20 20:51:41 -0500

    se agregan nuevas gráficas

--- RojasJuan_hw3.mk
+++ RojasJuan_hw3.mk
@@ -1,4 +1,6 @@
-all : PDE_initial.pdf PDE_fixed.pdf ODE.pdf
+all : PDE_initial.pdf PDE_fixed.pdf ODE.pdf ODE_additional.pdf
+
+RojasJuan_Plots_hw3.py : data_ODE.dat data_PDE_fixed.dat
 
 ODE.pdf : data_ODE.dat RojasJuan_Plots_hw3.py
 	python3 RojasJuan_Plots_hw3.py
@@ -13,3 +15,6 @@ PDE_initial.pdf : data_PDE_fixed.dat RojasJuan_Plots_hw3.py
 data_PDE_fixed.dat: RojasJuan_PDE.cpp
 	g++ $^
 	./a.out
+
+ODE_additional.pdf : data_ODE.dat RojasJuan_Plots_hw3.py
+	python3 RojasJuan_Plots_hw3.py
